fa4b931aae690d4f4221f6ea82aad9790ed93966
Alex Ballera <alexballera@gmail.com>
2016-03-25 22:32:34 -0430

    Optimizando tema

--- src/header.php
+++ src/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
